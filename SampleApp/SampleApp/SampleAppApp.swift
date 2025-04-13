28d221f517a49c4f17c92e07ebde9b7ca3999984
kurisu-seima <74137008+kurisu-seima@users.noreply.github.com>
2025-04-13 21:58:12 +0900

    サンプルプロジェクト追加

---
+++ SampleApp/SampleApp/SampleAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SampleAppApp.swift
+//  SampleApp
+//
+//  Created by 栗須　星舞 on 2025/04/13.
+//
+
+import SwiftUI
+
+@main
+struct SampleAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
